aa530fe9378c6a3d498a4a884332e2b2db443b99
Hamed <hamedsedighi@gmail.com>
2024-08-29 10:43:25 +0200

    update local envs

--- app/action/submit-form.ts
+++ app/action/submit-form.ts
@@ -2,7 +2,7 @@
 
 export const submitForm = async (url: string, language?: string) => {
   try {
-    const res = await fetch('http://localhost:3000/api/roast', {
+    const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/roast`, {
       method: 'POST',
       body: JSON.stringify({ url, language }),
     })
--- utils/generate-roast.ts
+++ utils/generate-roast.ts
@@ -8,7 +8,7 @@ export const generateRoast = async (
     'You create playful ridicule and roasts of CouchSurfer member based on their profile information. Keep it fun, humorous, and brief'
   const prompt = `Write a short and playful roast in ${language} for the following CouchSurfer: ${couchsurferInfo}`
 
-  const genAI = new GoogleGenerativeAI(process.env.API_KEY!)
+  const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY!)
   const model = genAI.getGenerativeModel({
     model: 'gemini-1.5-flash',
     systemInstruction,
